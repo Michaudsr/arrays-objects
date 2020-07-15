042f689ae85fe501148072644936a952eb57c7b7
Steven Michaud <stevenmichaud@Stevens-MacBook-Air.local>
2020-07-15 17:03:59 -0400

    [arrays] Add favorite teams

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = "Steven";
+console.log(name)
+
+var myLocation = "Atlanta"
+console.log(myLocation);
+
+var favoriteNumber = 3;
+console.log(favoriteNumber);
+
+var teams = ["Rockets","Lakers", "Warriors", "Clippers", "Blazers","Celtics","Nets"]
+console.log(teams);
+
