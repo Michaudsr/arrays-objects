8b6c7825d914b8ea0a17f36e414e14a7a2504f11
Steven Michaud <stevenmichaud@Stevens-MacBook-Air.local>
2020-07-15 20:22:19 -0400

    [steven] function expression

--- app.js
+++ app.js
@@ -119,3 +119,63 @@ const steven = {
 // likes, captions, shares, comments, username, picture
 
 // instagram post object 
+
+function sayHello() {
+    //write my code
+    //write your logic
+    console.log("Hello");
+}
+
+    sayHello();
+    sayHello();
+
+    // function expression
+    const sayHelloAgain = function(){
+        console.log("Hello Again");
+
+    };
+
+    sayHelloAgain();
+
+    arrow function
+
+    const sayHelloFinal = () => {
+        console.log("Hello for the last time");
+
+    }
+    
+    sayHelloFinal();
+
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const otherNumbers = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+    function printNumbers(array) {
+        for (let i = 0; i < array.length; i++) {
+            let num = array[i]; // 10, 20, 30
+            
+            if (num < 50) {
+                console.log(" This number is less than 50: " + num)
+            } else{
+                console.log("This number is greater than 50: " + num);
+            }
+            
+
+        }
+    }
+printNumbers(numbers);
+printNumbers(otherNumbers);
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function addNumbers(array) {
+    let result = 0;
+
+    for( let i = 0; i < array.length; i++){
+        let num = array[i];//1, 2, 3, 4, 5
+
+        result += num; // 1, 3, 6, 10, 15, ...
+    }
+    return result;
+}
+
+addNumbers(myNumbers)
